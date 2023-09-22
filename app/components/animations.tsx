4dbaafa5703087ff917468d1846a14275781078b
ibelick <julien.thibeaut@gmail.com>
2023-09-22 18:49:00 +0200

    feat: add more animations

--- app/components/animations.tsx
+++ app/components/animations.tsx
@@ -80,3 +80,163 @@ export const ZoomOut = () => {
     </ContainerAnimation>
   );
 };
+
+export const Rotate90 = () => {
+  return (
+    <ContainerAnimation>
+      <div className={`${globalCn} animate-rotate-90`} />
+    </ContainerAnimation>
+  );
+};
+
+export const Rotate180 = () => {
+  return (
+    <ContainerAnimation>
+      <div className={`${globalCn} animate-rotate-180`} />
+    </ContainerAnimation>
+  );
+};
+
+export const Rotate360 = () => {
+  return (
+    <ContainerAnimation>
+      <div className={`${globalCn} animate-rotate-360`} />
+    </ContainerAnimation>
+  );
+};
+
+export const FlipHorizontal = () => {
+  return (
+    <ContainerAnimation>
+      <div className={`${globalCn} animate-flip-horizontal`} />
+    </ContainerAnimation>
+  );
+};
+
+export const FlipVertical = () => {
+  return (
+    <ContainerAnimation>
+      <div className={`${globalCn} animate-flip-vertical`} />
+    </ContainerAnimation>
+  );
+};
+
+export const Bounce = () => {
+  return (
+    <ContainerAnimation>
+      <div className={`${globalCn} animate-bounce`} />
+    </ContainerAnimation>
+  );
+};
+
+export const Swing = () => {
+  return (
+    <ContainerAnimation>
+      <div className={`${globalCn} animate-swing`} />
+    </ContainerAnimation>
+  );
+};
+
+export const Wobble = () => {
+  return (
+    <ContainerAnimation>
+      <div className={`${globalCn} animate-wobble`} />
+    </ContainerAnimation>
+  );
+};
+
+export const Pulse = () => {
+  return (
+    <ContainerAnimation>
+      <div className={`${globalCn} animate-pulse`} />
+    </ContainerAnimation>
+  );
+};
+
+export const Shake = () => {
+  return (
+    <ContainerAnimation>
+      <div className={`${globalCn} animate-shake`} />
+    </ContainerAnimation>
+  );
+};
+
+export const Tada = () => {
+  return (
+    <ContainerAnimation>
+      <div className={`${globalCn} animate-tada`} />
+    </ContainerAnimation>
+  );
+};
+
+export const Jump = () => {
+  return (
+    <ContainerAnimation>
+      <div className={`${globalCn} animate-jump`} />
+    </ContainerAnimation>
+  );
+};
+
+export const Hang = () => {
+  return (
+    <ContainerAnimation>
+      <div className={`${globalCn} animate-hang`} />
+    </ContainerAnimation>
+  );
+};
+
+export const RollIn = () => {
+  return (
+    <ContainerAnimation>
+      <div className={`${globalCn} animate-roll-in`} />
+    </ContainerAnimation>
+  );
+};
+
+export const RollOut = () => {
+  return (
+    <ContainerAnimation>
+      <div className={`${globalCn} animate-roll-out`} />
+    </ContainerAnimation>
+  );
+};
+
+export const Float = () => {
+  return (
+    <ContainerAnimation>
+      <div className={`${globalCn} animate-float`} />
+    </ContainerAnimation>
+  );
+};
+
+export const Sink = () => {
+  return (
+    <ContainerAnimation>
+      <div className={`${globalCn} animate-sink`} />
+    </ContainerAnimation>
+  );
+};
+
+export const Flash = () => {
+  return (
+    <ContainerAnimation>
+      <div className={`${globalCn} animate-flash`} />
+    </ContainerAnimation>
+  );
+};
+
+export const Jiggle = () => {
+  return (
+    <ContainerAnimation>
+      <div className={`${globalCn} animate-jiggle`} />
+    </ContainerAnimation>
+  );
+};
+
+export const RubberBand = () => {
+  return (
+    <ContainerAnimation>
+      <div className={`${globalCn} animate-rubber-band`} />
+    </ContainerAnimation>
+  );
+};
